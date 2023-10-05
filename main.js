0ee2c33bce065eb6ad5b948c645a39cf149a9da9
danettik <123102934+danettik@users.noreply.github.com>
2023-10-05 13:10:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,20 +5,25 @@ function validaNome(nomeCompleto) { // Corrected function name
     return nomeComoArray.length >= 2;
 }
 
-function 
+function palpiteRifa(primeiroPalpite, segundoPalpite){
+    return primeiroPalpite < segundoPalpite;
+}
 
 form.addEventListener('submit', function(e) { //"e" é de evento
     e.preventDefault(); //Impede que o formulário seja carregado antes da hora
 
-        let validaForm = (primeiroPalpite.value, segundoPalpite.value);
-        if(validaForm){
+        let validarForm = (primeiroPalpite.valueAsNumber, segundoPalpite.valueAsNumber);
+        if(validarForm){
             alert("Palpite enviado. Boa sorte!")
+        } else {
+            alert("O número do primeiro palpite não pode ser maior que o segundo")
         }
 
-    const seuNome = document.getElementById('seu-nome');
+    const seuNome = document.getElementById('seu-nome'); // Recupera informação
     const primeiroPalpite = document.getElementById('primeiro-palpite');
     const segundoPalpite = document.getElementById('segundo-palpite');
     const mensagemSucesso = 'Palpite enviado com sucesso!';
+    const erro = document.getElementById('erro');
 
     formEValido = validaNome(seuNome.value); // Corrected function name
     if (formEValido) {
